c17f9c61b23f42d803a24fdd664c5ebdef87fdf6
Julius O <j.osokinas@mappingforchange.org.uk>
2015-11-04 15:26:44 +0000

    v0.1.0-beta.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='geokey-airquality',
-    version='0.1.0',
+    version='0.1.0-beta.1',
     author='Julius Osokinas',
     author_email='j.osokinas@mappinforchange.org.uk',
     packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
