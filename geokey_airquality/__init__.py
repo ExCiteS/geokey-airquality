bd051d385f92f91503492d0d22ea0965b4924ae1
Julius O <j.osokinas@mappingforchange.org.uk>
2015-11-26 15:44:00 +0000

    Better setup

--- geokey_airquality/__init__.py
+++ geokey_airquality/__init__.py
@@ -1,6 +1,9 @@
 from geokey.extensions.base import register
 
 
+VERSION = (0, 0, 0)
+__version__ = '.'.join(map(str, VERSION))
+
 register(
     'geokey_airquality',
     'Air Quality',
--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+#!/usr/bin/env python
+
+from os.path import join
 from setuptools import setup, find_packages
 
 
+name = 'geokey-airquality'
+version = __import__(name.replace('-', '_')).__version__
+repository = join('https://github.com/ExCiteS', name)
+
 setup(
-    name='geokey-airquality',
-    version='0.1.0-beta.1',
+    name=name,
+    version=version,
+    description='GeoKey extension for Air Quality functionality',
+    url=repository,
+    download_url=join(repository, 'tarball', version),
     author='Julius Osokinas',
     author_email='j.osokinas@mappinforchange.org.uk',
+    license='MIT',
     packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
     include_package_data=True,
+    install_requires=[],
 )
