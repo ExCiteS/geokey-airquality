5f26ab43faf3552e8ad9e46cbb876044a4daab53
Julius O <j.osokinas@mappingforchange.org.uk>
2016-04-18 14:31:34 +0100

    v1.0.2

--- geokey_airquality/__init__.py
+++ geokey_airquality/__init__.py
@@ -1,13 +1,18 @@
-from geokey.extensions.base import register
+"""Main initialisation for extension."""
 
-
-VERSION = (1, 0, 1)
+VERSION = (1, 0, 2)
 __version__ = '.'.join(map(str, VERSION))
 
-register(
-    'geokey_airquality',
-    'Air Quality',
-    display_admin=True,
-    superuser=True,
-    version=__version__
-)
+
+try:
+    from geokey.extensions.base import register
+
+    register(
+        'geokey_airquality',
+        'Air Quality',
+        display_admin=True,
+        superuser=True,
+        version=__version__
+    )
+except BaseException:
+    print 'Please install GeoKey first'
